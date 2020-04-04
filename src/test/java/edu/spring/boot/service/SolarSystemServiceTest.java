59e97e26fd3ad6437744096c5296843d7134c3d6
mkvisu <48646246+mkvisu@users.noreply.github.com>
2020-04-04 17:56:51 +0530

    testcase update

--- src/test/java/edu/spring/boot/service/SolarSystemServiceTest.java
+++ src/test/java/edu/spring/boot/service/SolarSystemServiceTest.java
@@ -1,5 +1,7 @@
 package edu.spring.boot.service;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +21,7 @@ public class SolarSystemServiceTest {
 	@Test
 	public void testGetInformationTest() {
 		SolarSystem record = service.getInformation();
-
+		assertTrue("Incorrect", record.getLocated().equalsIgnoreCase("Milky Way"));
 		System.out.println(record);
 	}
 }
